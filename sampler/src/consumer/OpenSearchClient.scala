db1cf60ec24d752b14752b0585934d539fcb58b2
Grzegorz Bielski <pesiok@gmail.com>
2023-06-10 16:58:51 +0200

    do not stop indexing at single ser err

--- sampler/src/consumer/OpenSearchClient.scala
+++ sampler/src/consumer/OpenSearchClient.scala
@@ -14,21 +14,26 @@ import cats.effect.kernel.Resource
 import cats.effect.IO
 import scala.jdk.CollectionConverters.*
 import cats.syntax.all.*
-import cats.implicits.*
 import cats.effect.kernel.Async
 import cats.effect.kernel.Sync
 import cats.ApplicativeError
 import cats.MonadThrow
 import cats.Show
 import org.opensearch.client.opensearch._types.ErrorCause
-import OpenSearchClient.{*, given}
 import org.opensearch.client.json.JsonpSerializer
 import jakarta.json.Json
 import jakarta.json.stream.JsonGenerator
 import org.opensearch.client.json.{JsonpMapper, JsonpSerializable}
 import java.io.StringReader
+import jakarta.json.JsonValue
+import scala.util.Try
+import org.typelevel.log4cats.syntax.*
+import cats.data.*
+import org.typelevel.log4cats.Logger
 
-final class OpenSearchClient[F[_]: Async](client: OpenSearchAsyncClient):
+import OpenSearchClient.{*, given}
+
+final class OpenSearchClient[F[_]: Async: Logger](client: OpenSearchAsyncClient):
   def indexExists(name: String): F[Boolean] =
     suspendFuture:
       client.indices.exists(_.index(name))
@@ -39,27 +44,32 @@ final class OpenSearchClient[F[_]: Async](client: OpenSearchAsyncClient):
       client.indices.create(_.index(name))
 
   def bulkAdd(index: String, docs: Vector[String]): F[BulkResponse] =
-    suspendFuture:
-      val ops = docs
-        .map: doc =>
+    docs
+      .map(stringSerializer)
+      .collect:
+        // ignoring serialization errors
+        case Right(value) => value.pure[F]
+      .traverse:
+        _.map: doc =>
           BulkOperation
             .Builder()
-            .index:
-              _.index(index)
-                  // TODO: handle serialisation errors in StringSerializer, remove root cause
-                .document(StringSerializer(doc))
+            .index(_.index(index).document(doc))
             .build
-        .asJava
-      client.bulk(_.operations(ops))
-    .reject:
-      case response if response.errors =>
-        val errors = response.items.asScala.map(_.error).toVector
-        val report =
-          if errors.distinctBy(_.`type`).size == 1 then errors.head.show else errors.show
+      .flatMap: ops =>
+        suspendFuture:
+          client.bulk(_.operations(ops.asJava))
+      .reject:
+        case response if response.errors =>
+          val errors = response.items.asScala.map(_.error).toVector
+          val report =
+            if errors.distinctBy(_.`type`).size == 1 then errors.head.show else errors.show
 
-        RuntimeException(s"Failed to index documents:\n $report")
+          RuntimeException(s"Failed to index documents:\n $report")
 
 object OpenSearchClient:
+  enum OpenSearchError extends Exception:
+    case BulkSerializationError(causes: NonEmptyChain[String])
+
   inline given Show[ErrorCause] = Show.show: cause =>
     s"""ErrorCause(
     |type = ${cause.`type`},
@@ -68,7 +78,7 @@ object OpenSearchClient:
     """.stripMargin
 
   // TODO: parametrize host and credentials
-  def resource[F[_]: Async]: Resource[F, OpenSearchClient[F]] =
+  def resource[F[_]: Async: Logger]: Resource[F, OpenSearchClient[F]] =
     Resource
       .fromAutoCloseable:
         Sync[F].delay:
@@ -90,11 +100,11 @@ object OpenSearchClient:
   private def suspendFuture[F[_]: Async, A](fut: => CompletableFuture[A]): F[A] =
     Async[F].fromCompletableFuture(Sync[F].delay(fut))
 
-// see:
-// - https://github.com/yash025/opensearch-scala-demo/blob/master/src/main/scala/CirceToJava.scala
-// - https://github.com/opensearch-project/opensearch-java/issues/362
-final class StringSerializer(value: String) extends JsonpSerializable:
-  override def serialize(generator: JsonGenerator, mapper: JsonpMapper): Unit =
-    val jsonReader = Json.createReader(StringReader(value))
-    val messageAsJson = jsonReader.read()
-    generator.write(messageAsJson)
+  // OpenSearch client doesn't provide a way to pass JSON string directly
+  // see: https://github.com/yash025/opensearch-scala-demo/blob/master/src/main/scala/CirceToJava.scala
+  private def stringSerializer(value: String): Either[Throwable, JsonpSerializable] =
+    Try(Json.createReader(StringReader(value)).read()).toEither
+      .map: value =>
+        new JsonpSerializable:
+          override def serialize(generator: JsonGenerator, _mapper: JsonpMapper): Unit =
+            generator.write(value)
